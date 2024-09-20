ec1662778830dacbf8e3b0a8e1ae7badad829957
frohlichramon01 <frohlichramon01@gmail.com>
2024-09-19 21:48:50 -0300

    finishes homework

--- main.js
+++ main.js
@@ -6,12 +6,24 @@ function Animal(name, color, age, weight){
 }
 
 
-function Dog(breed){
+function Dog(breed, name, color, age, weight){
     this.breed = breed;
+
+    Animal.call(this, name, color, age, weight);
+
+    this.woof = function(){
+        console.log(this.name + ': woof woof');
+    }
 }
 
-function Cat(breed){
+function Cat(breed, name, color, age, weight){
     this.breed = breed;
+
+    Animal.call(this, name, color, age, weight);
+
+    this.meow = function(){
+        console.log(this.name + ': meow meow');
+    }
 }
 
 
@@ -19,3 +31,8 @@ const dolce = new Dog('Dolce', 'Brown', 'Can turner', 10, 18);
 const hera = new Dog('Hera', 'Brown', 'Can turner', 7, 14);
 const gaia = new Dog('Gaia', 'Yellow', 'Can turner', 5, 15);
 const soneca = new Cat('Soneca', 'Gray', 'Siamese', 5, 7);
+
+console.log(dolce);
+console.log(hera);
+console.log(gaia);
+console.log(soneca);
